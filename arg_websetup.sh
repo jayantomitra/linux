514b504b1205e80929a08c76069074b8c613fde1
Jayanto Mitra <jayantomitra911@gmail.com>
2022-06-06 19:06:36 +0000

    shell script taking arguments

---
+++ arg_websetup.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+
+#VARIABLE DECLARATION
+
+PACKAGE="httpd wget unzip"
+SRVC="httpd"
+TEMP_DIR="/tmp/webfiles"
+# SRC_URL="https://www.tooplate.com/zip-templates/2128_tween_agency.zip"
+#ARTIFACT="2128_tween_agency"
+
+ 
+echo "install packages"
+
+echo "Installing packages"
+
+sudo yum install $PACKAGE -y > /dev/null
+
+echo "###############################################"
+echo "start enable services"
+sudo systemctl start $SRVC
+sudo systemctl enable $SRVC
+
+
+echo
+echo "##############################################"
+echo "Making temp directories"
+
+mkdir -p $TEMP_DIR 
+
+cd $TEMP_DIR 
+
+wget $1
+
+unzip $2.zip
+
+sudo cp -r $2/* /var/www/html/
+
+echo "##############################################"
+
+echo "restart start services"
+systemctl restart $SRVC
+
+echo
+echo "remove temp files"
+
+rm -rf $TEMP_DIR
+
+
+
+
+
