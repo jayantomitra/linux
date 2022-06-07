4f8c7e2a3e4e9cdf66930e7ab03b531cb2fd1e50
Jayanto Mitra <jayantomitra911@gmail.com>
2022-06-07 16:57:08 +0000

    input scripts taking inputs from user

---
+++ input_script.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo
+echo "Enter your skill"
+read SKILL
+
+echo
+echo "Your skill $SKILL is in high demand"
+echo
+echo "************************************"
+
+read -p 'USERNAME: ' USR
+read -sp 'PASSWORD: ' pass
+
+echo
+
+echo "Login Successfull : Welcome USER $USR"
